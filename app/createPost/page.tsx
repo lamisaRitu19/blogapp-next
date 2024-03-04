306836814ca904066ac017c1fe70335ad25a1c4b
Lamisa Ferdous Ritu <lamisaferdous19@gmail.com>
2024-03-04 19:03:33 +0600

    second commit

--- app/createPost/page.tsx
+++ app/createPost/page.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>Create post page</div>
+    <div className="pb-10 bg-slate-200">
+      <div className='container mx-auto'>
+        <h2 className="text-2xl text-center font-semibold pt-10 mb-6">Create a new post</h2>
+        <form>
+          <input placeholder='Title' className='w-full px-4 py-1'></input>
+        </form>
+      </div>
+    </div>
   )
 }
