de1a91477c3920e753fc4c90dca5398274c719ac
Lamisa Ferdous Ritu <lamisaferdous19@gmail.com>
2024-03-06 19:02:32 +0600

    auth added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,38 +1,57 @@
+"use client"
+
+import { getCurrentUser } from 'aws-amplify/auth'
+import { Hub } from 'aws-amplify/utils';
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Navbar() {
-    const navItems = [
-        {
-            id: 1,
-            title: "Home",
-            url: "/"
-        },
-        {
-            id: 2,
-            title: "Create Post",
-            url: "/createPost"
-        },
-        {
-            id: 3,
-            title: "Profile",
-            url: "/profile"
-        },
-        {
-            id: 4,
-            title: "My Post",
-            url: "/myPost"
-        },
-    ]
-    return (
-        <div className='flex justify-center space-x-6 border-b py-3 bg-cyan-500 border-gray-300'>
-            {
-                navItems.map(item => <Link 
-                    href={item.url} 
-                    key={item.id}
-                    className='text-lg font-medium rounded-lg px-3 py-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900'
-                >{item.title}</Link>)
+    const [signedUser, setSignedUser] = useState(false);
+    useEffect(() => {
+        authListener();
+    }, [])
+
+    const authListener = async() => {
+        Hub.listen('auth', ({ payload }) => {
+            switch (payload.event) {
+              case 'signedIn':
+                setSignedUser(true);
+                break;
+              case 'signedOut':
+                setSignedUser(false);
+                break;
             }
+        });
+        try {
+            const {username, userId, signInDetails} = await getCurrentUser()
+            setSignedUser(true);
+        } catch (error) {
+            console.log("🚀 ~ authListener ~ error:", error)            
+        }
+    }
+
+    return (
+        <div className='flex justify-center space-x-6 border-b py-3 bg-cyan-500 border-gray-300'> 
+            <Link 
+                href="/"
+                className='text-lg font-medium rounded-lg px-3 py-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900'
+            >Home</Link>
+            {
+                signedUser && <>
+                    <Link 
+                        href="/createPost"
+                        className='text-lg font-medium rounded-lg px-3 py-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900'
+                    >Create Post</Link>
+                    <Link 
+                        href="/myPost"
+                        className='text-lg font-medium rounded-lg px-3 py-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900'
+                    >My Post</Link>
+                </>
+            }
+            <Link 
+                href="/profile"
+                className='text-lg font-medium rounded-lg px-3 py-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900'
+            >Profile</Link>
         </div>
     )
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+'use client'
 
-export default function page() {
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react'
+import React from 'react'
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from '@/app/aws-exports';
+Amplify.configure(awsExports);
+
+export default function Profile() {
   return (
-    <div className="container mx-auto my-10">
-      <h3 className="text-3xl font-semibold mb-3 text-cyan-900">Profile</h3>
-      <p className="text-lg mb-2 text-gray-500">Username:</p>
-      <p className="text-lg mb-6 text-gray-500">Email: </p>
-      <button className='font-semibold rounded-lg px-6 py-2 bg-red-800 text-white'>Sign Out</button>
-    </div>
+    <Authenticator className='pt-20'>
+      {
+        ({signOut, user}) => (
+          <div className="container mx-auto my-10">
+            <h3 className="text-3xl font-semibold mb-3 text-cyan-900">Profile</h3>
+            <p className="text-lg mb-2 text-gray-500">Username: {user?.username}</p>
+            <p className="text-lg mb-6 text-gray-500">Email: </p>
+            <button onClick={signOut} className='font-semibold rounded-lg px-6 py-2 bg-red-800 text-white'>Sign Out</button>
+          </div>
+        )
+      }
+    </Authenticator>
   )
 }
